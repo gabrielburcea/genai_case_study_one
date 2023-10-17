277fde82b4d1f1f3a88070079f1838a375906657
gabrielburcea <gabrielburcea>
2023-10-17 13:53:55 +0000

    adding extra info
    extra info

--- transforming_translating_grammar_etc.py
+++ transforming_translating_grammar_etc.py
@@ -97,11 +97,107 @@ for issue in user_messages:
 
 # COMMAND ----------
 
+""" Tone Transformation
+Writing can vary based on the intended audience. ChatGPT can produce different tones.
 
+"""
+
+prompt = f"""
+Translate the following from slang to a business letter: 
+'Dude, This is Joe, check out this spec on this standing lamp.'
+"""
+response = get_completion(prompt)
+print(response)
 
 # COMMAND ----------
 
+"""Format Conversion
+ChatGPT can translate between formats. The prompt should describe the input and output formats. """
 
+data_json = { "resturant employees" :[ 
+    {"name":"Shyam", "email":"shyamjaiswal@gmail.com"},
+    {"name":"Bob", "email":"bob32@gmail.com"},
+    {"name":"Jai", "email":"jai87@gmail.com"}
+]}
+
+prompt = f"""
+Translate the following python dictionary from JSON to an HTML \
+table with column headers and title: {data_json}
+"""
+response = get_completion(prompt)
+print(response)
+
+# COMMAND ----------
+
+from IPython.display import display, Markdown, Latex, HTML, JSON
+display(HTML(response))
+
+# COMMAND ----------
+
+"""Spellcheck/Grammar check.
+Here are some examples of common grammar and spelling problems and the LLM's response.
+
+To signal to the LLM that you want it to proofread your text, you instruct the model to 'proofread' or 'proofread and correct'. """
+
+# COMMAND ----------
+
+text = [ 
+  "The girl with the black and white puppies have a ball.",  # The girl has a ball.
+  "Yolanda has her notebook.", # ok
+  "Its going to be a long day. Does the car need it’s oil changed?",  # Homonyms
+  "Their goes my freedom. There going to bring they’re suitcases.",  # Homonyms
+  "Your going to need you’re notebook.",  # Homonyms
+  "That medicine effects my ability to sleep. Have you heard of the butterfly affect?", # Homonyms
+  "This phrase is to cherck chatGPT for speling abilitty"  # spelling
+]
+for t in text:
+    prompt = f"""Proofread and correct the following text
+    and rewrite the corrected version. If you don't find
+    and errors, just say "No errors found". Don't use 
+    any punctuation around the text:
+    ```{t}```"""
+    response = get_completion(prompt)
+    print(response)
+
+# COMMAND ----------
+
+text = f"""
+Got this for my daughter for her birthday cuz she keeps taking \
+mine from my room.  Yes, adults also like pandas too.  She takes \
+it everywhere with her, and it's super soft and cute.  One of the \
+ears is a bit lower than the other, and I don't think that was \
+designed to be asymmetrical. It's a bit small for what I paid for it \
+though. I think there might be other options that are bigger for \
+the same price.  It arrived a day earlier than expected, so I got \
+to play with it myself before I gave it to my daughter.
+"""
+prompt = f"proofread and correct this review: ```{text}```"
+response = get_completion(prompt)
+print(response)
+
+# COMMAND ----------
+
+from redlines import Redlines
+
+diff = Redlines(text,response)
+display(Markdown(diff.output_markdown))
+
+# COMMAND ----------
+
+prompt = f"""
+proofread and correct this review. Make it more compelling. 
+Ensure it follows APA style guide and targets an advanced reader. 
+Output in markdown format.
+Text: ```{text}```
+"""
+response = get_completion(prompt)
+display(Markdown(response))
+
+# COMMAND ----------
+
+Thanks to the following sites:
+
+https://writingprompts.com/bad-grammar-examples/
 
 # COMMAND ----------
 
